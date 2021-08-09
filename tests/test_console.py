251d3147efb3cb04d3e588db03478c67121eed93
Juansu01 <thejuank0@hotmail.com>
2021-08-09 18:04:38 -0500

    Adding file for console testing

---
+++ tests/test_console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This module tests our console"""
+import os
+from io import StringIO
+import unittest
+from console import HBNBCommand
+from unittest.mock import patch
+
+
+class Test_console(unittest.TestCase):
+    """This class tests our console"""
+    @classmethod
+    def setUpClass(self):
+        """ Sets the class for testing"""
+        self.consola1 = HBNBCommand()
+
+    @classmethod
+    def tearDownClass(self):
+        """ This deletes the class we created """
+        del self.consola1
+
+    def test_create(self):
+        """ Testing create method """
+        with patch('sys.stdout', new=StringIO()) as mock:
+            self.consola1.onecmd("create")
+            self.assertEqual(
+                "** class name missing **\n", mock.getvalue())

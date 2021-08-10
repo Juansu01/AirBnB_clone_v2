c8730a51232dd62a4fb82486a9faaa925526a631
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-10 11:18:38 -0500

    add new public instance method, def delete obj

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,8 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+    def delete(self, obj=None):
+        if object == None:
+            pass
+        else:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]

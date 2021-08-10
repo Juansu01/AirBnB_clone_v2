1e4ba8e480fad1ee4bc1247cfd1b1a7b9809705d
Juansu01 <thejuank0@hotmail.com>
2021-08-10 09:54:24 -0500

    Adding test case

--- tests/test_console.py
+++ tests/test_console.py
@@ -22,5 +22,10 @@ class TestConsole(unittest.TestCase):
     def test_do_create(self):
         """ Testing create method """
         with patch('sys.stdout', new=StringIO()) as mock:
-            self.consola1.onecmd("create State")
+            self.consola1.onecmd("create State numb=2")
             self.assertTrue(len(mock.getvalue()) >= 1)
+
+
+        with patch('sys.stdout', new=StringIO()) as mock2:
+            self.consola1.onecmd("show State " + mock.getvalue())
+            self.assertTrue("numb" in mock2.getvalue())

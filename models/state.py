40433f9579a03ea935bd85d864ebf1c2cddd564f
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-11 12:12:54 -0500

    update state class that inherits from baseModel, tast 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from AirBnB_clone_v2.models.city import City
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+from models.engine.file_storage import all
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__= 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """lis of cities with satate_id equal to current id"""
+            list_city = []
+            dic_obj = all(City)
+            for city in list(dic_obj).values():
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
